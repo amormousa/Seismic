9177a28aaf0e049b9b88b086c33f58b45b9d23d2
Muhannad H. <me@muhannad.cc>
2026-07-04 08:51:45 +0300

    feat(web): configure http client, styles, and root routing

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/auth/login/login';
+import { Verify } from './pages/auth/verify/verify';
+import { Dashboard } from './pages/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: Login },
+  { path: 'verify', component: Verify },
+  { path: 'dashboard', component: Dashboard },
+];
